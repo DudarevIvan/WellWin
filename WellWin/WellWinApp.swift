9ebd849b5542dee22b61d4cf65fe501daf617f30
DudarevIvan <33264616+DudarevIvan@users.noreply.github.com>
2021-07-14 00:04:06 +0500

    Initial Commit

---
+++ WellWin/WellWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellWinApp.swift
+//  WellWin
+//
+//  Created by Ivan Dudarev on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct WellWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
